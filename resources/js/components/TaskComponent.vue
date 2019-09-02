03ca01f75e00b6b05896030638429ef9bf10f5be
ikegawa-t <ikegawataichi56@gmail.com>
2019-09-02 21:38:03 +0900

    Vue scriptのリファクタリング

--- resources/js/components/TaskComponent.vue
+++ resources/js/components/TaskComponent.vue
@@ -3,12 +3,12 @@
     <div class="col-sm-offset-2 col-sm-8">
 
       <!-- クリック後　New Task 表示 -->
-      <button v-on:click="openDiv()" type="submit" class="btn title" style="background-color:#f5f5f5; margin-bottom: 2vh; color: black; border-color:#dcdcdc;">
+      <button @click="openDiv()" type="submit" class="btn title" style="background-color:#f5f5f5; margin-bottom: 2vh; color: black; border-color:#dcdcdc;">
         <i class="fa fa-pencil-square-o" aria-hidden="true"></i> New Task
       </button>
 
       <!-- クリック後　Deleted task 表示 -->
-      <button v-on:click="closeDiv()" type="submit" class="btn title" style="margin-bottom: 2vh; background-color:#E0E0E0; color: black; border-color: #dcdcdc;">
+      <button @click="closeDiv()" type="submit" class="btn title" style="margin-bottom: 2vh; background-color:#E0E0E0; color: black; border-color: #dcdcdc;">
         <i class="fa fa-btn fa-trash"></i>Deleted Task
       </button>
 
@@ -76,7 +76,7 @@
           <!--   -->
           <div class="form-group">
             <label for="task-name" class="col-sm-3 control-label">Selection&nbsp;&nbsp;:&nbsp;&nbsp;
-              <select name="selection" v-model.trim="selection" placeholder="keyword" @click="resetseachText()">
+              <select name="selection" v-model.trim="selection" placeholder="keyword" @click="resetSeachText()">
                 <option value="text">Task</option>
                 <option value="time">Date</option>
               </select>
@@ -196,131 +196,125 @@ button:active{
   export default {
     data: function() {
       return {
-        //タスク履歴とタスク追加ページの表示切り替え
+          //タスク履歴とタスク追加ページの表示切り替え
         open: true,
         //searchするカテゴリ
         selection: 'text',
         //searchする文字列
         keyword: '',
-        //ソートするアイテム
+        //seachする
         seachText:'',
         //ソートするアイテム
         sort: {
           key: '',
           isAsc: false,
-              },
-              //入力フォームがaddtextに入る。
+        },
+        //入力フォームがaddtextに入る。
         addtext:'',
         // todos 追加要素の見込みがある為{}使用
         todos: [
-          { text:'AM会議' },
-          { text:'見積書提出' }
-          ],
-          compTask:[],
-        }
-      },
+          {text:'AM会議'},
+          {text:'見積書提出'}
+        ],
+        compTask:[],
+      }
+    },
     computed: {
       sortedItems () {
-        var list= this.findBy(this.compTask, this.keyword, this.selection); 
-        if (this.sort.key) {
-          list.sort((a, b) => {
-            a = a[this.sort.key]
-            b = b[this.sort.key]
-           return (a === b ? 0 : a > b ? 1 : -1) * (this.sort.isAsc ? 1 : -1)
-          });
-        }
-        return list;
+        //サーチした物を一旦itemに入れる。
+        var item= this.findBy(this.compTask, this.keyword, this.selection); 
+          if (this.sort.key) {
+            //クイックソートを使用して並び替え
+            item.sort((a, b) => {
+              a = a[this.sort.key]
+              b = b[this.sort.key]
+              return (a === b ? 0 : a > b ? 1 : -1) * (this.sort.isAsc ? 1 : -1)
+            });
+          }
+        return item;
       }
     },
     methods: {
       //validateにエラーがない場合 todosへ追加後　addtextを空にする。その後、ローカルストレージに保存
-      addToDo: function() {
+      addToDo() {
         //注！！アロー関数(引数)=>{処理}
         this.$validator.validateAll().then((result) => {
           if (result) {
             if (this.addtext) {
               this.todos.push({ text:this.addtext});
               this.addtext = '';
-              this.saveTodo();
-            }
-          }
-        });
-      },                          
-      cleanToDo: function(todo) {
-        todo.time = moment(new Date).format('YYYY/MM/DD HH:mm');
-        this.compTask.push(todo);
-        this.todos.splice(this.todos.indexOf(todo),1);
-        this.saveCompTask();
-        this.saveTodo();
-      },
-      seachTodo:function(){
-        this.$validator.validateAll().then((result) => {
-          if (result) {
-            if(this.selection == 'time'){
-              // input deteの中身が YYYY-MM-DD なので、YYYY/MM/DD に置換する！！ 全体の　- を　/ にするため、正規表現を使用
-              this.keyword = this.seachText.replace(/-/g, '/');
-            }else{
-              this.keyword = this.seachText;
+              this.saveBy('todos',this.todos);
             }
           }
         })
       },
-      //ソート機能 
-      sortBy (key) {
-        this.sort.isAsc = this.sort.key === key ? !this.sort.isAsc : false;
-        this.sort.key = key;
+      seachTodo(){
+        this.$validator.validateAll().then((result) => {
+        if (result) {
+          if(this.selection == 'time'){
+            // input deteの中身が YYYY-MM-DD なので、YYYY/MM/DD に置換する！！正規表現
+            this.keyword = this.seachText.replace(/-/g, '/');   
+          }else{
+            this.keyword = this.seachText;
+          }
+        }
+        })
       },
-      //検索機能
-      findBy: function (list, value, column) {
-        return list.filter(function (item) {
-          // 入力がない場合は全件表示 valueを含む文字を検索に変更　
-          return (item[column].indexOf(value) > -1  || value === '')
-         })
-        },
-      //表示の切り替え
-      openDiv:function(){
-        this.open = true ;
-      },
-      closeDiv:function(){
-        this.open = false ;
-      },
-      //項目の切り替え後　検索ワードをリセット
-      resetseachText:function(){
-        this.seachText = '';
-      },
-      //localStorage に保存　（オブジェクトや配列はそのままJSONで扱えないので、stringifyでエンコードする。）
-      saveTodo: function(){
-        localStorage.setItem('todos', JSON.stringify(this.todos));
-      },
-      //localStorage に保存　（DeleteしたTaskを保存）
-      saveCompTask:function(){
-        localStorage.setItem('compTask', JSON.stringify(this.compTask));
-      },
-      //localStorageから　ロードする。前回とは逆にJSONから、parseでデコードする。
-      loadTodo: function(){
-        this.todos = JSON.parse( localStorage.getItem('todos') );
-        if( !this.todos ){
-          this.todos = [];
-        } 
+      //todos 完了したタスクをitemsに配置する。                           
+      cleanToDo(todo) {
+        todo.time = moment(new Date).format('YYYY/MM/DD HH:mm');
+        this.compTask.push(todo);
+        this.todos.splice(this.todos.indexOf(todo),1);
+        this.saveBy('compTask',this.compTask);
+        this.saveBy('todos',this.todos);
       },
       //削除履歴のリセット　confirmがokなら削除
-      resetCompTask:function(){
+      resetCompTask(){
         if(window.confirm('履歴のリセットを行います')){
           localStorage.removeItem('compTask');
           this.loadCompTask();
         }
       },
-      loadCompTask: function(){
-        this.compTask = JSON.parse( localStorage.getItem('compTask') );
-        if( !this.compTask ){
-          this.compTask = [];
-        } 
+      //検索ワードのSelectionが変更された時に日付検索にテキストの検索文字が入らないようにするため、切り替わる度にリセットする。
+      resetSeachText(){
+        this.seachText = '';
       },
-    },
-    //プラウザで開いた時にloadTodoしてロードするようにする。
-    mounted: function(){
-      this.loadTodo();
-      this.loadCompTask();
+      //表示の切り替え　(New Task)
+      openDiv(){
+        this.open = true ;
+      },
+      //表示の切り替え　(Delete Task)
+      closeDiv(){
+        this.open = false ;
+      },
+      //ソート機能
+      sortBy(key) {
+        this.sort.isAsc = this.sort.key === key ? !this.sort.isAsc : false;
+        this.sort.key = key;
+      },
+      //検索機能
+      findBy(list, value, column) {
+        return list.filter((item) => {
+        // 入力がない場合は全件表示
+        return (item[column].indexOf(value) > -1  || value === '')
+        })
+      },
+      //localStorage に保存　（オブジェクトや配列はそのままJSONで扱えないので、stringifyでエンコードする。）saveBy（保存名、保存するアイテム）
+      saveBy(name,item){
+        localStorage.setItem(name, JSON.stringify(item));
+      },
+      //localStorageから　ロードする。前回とは逆にJSONから、parseでデコードする。
+      loadBy(item,name){
+        item = JSON.parse( localStorage.getItem(name) );
+          if( !item ){
+            item = [];
+          } 
+        },
+      },
+      //プラウザで開いた時にloadTodoしてロードするようにする。
+      mounted(){
+        this.loadBy(this.compTask,'compTask');
+        this.loadBy(this.todos,'todos');
+      },
     }
-  }
 </script>
